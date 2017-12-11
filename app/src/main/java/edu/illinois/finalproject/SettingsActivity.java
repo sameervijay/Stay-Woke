019bb663920d143ac197e972555edfa9139490ce
sameervijay <sameer.vijay4@gmail.com>
2017-12-11 17:51:52 -0600

    Getting and displaying linked user's last trip works

--- app/src/main/java/edu/illinois/finalproject/SettingsActivity.java
+++ app/src/main/java/edu/illinois/finalproject/SettingsActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 //import com.firebase.ui.auth.AuthUI;
 //import com.firebase.ui.auth.IdpResponse;
 //import com.firebase.ui.auth.ResultCodes;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
@@ -23,7 +25,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -32,7 +37,8 @@ public class SettingsActivity extends AppCompatActivity {
     private boolean isSignedIn;
 
     private Button signInButton, linkAccountButton, createAccountButton;
-    private TextView nameTextView, emailTextView, linkedTextView, lastTripTextView, durationTextView, alarmsTextView;
+    private TextView nameTextView, emailTextView;
+    private TextView linkedToTextView, linkedTextView, lastTripTextView, durationTextView, alarmsTextView;
 
     private static final int RC_SIGN_IN = 123;
     private FirebaseAuth firebaseAuth;
@@ -52,6 +58,8 @@ public class SettingsActivity extends AppCompatActivity {
         linkAccountButton = (Button) findViewById(R.id.linkAccountButton);
         nameTextView = (TextView) findViewById(R.id.nameTextView);
         emailTextView = (TextView) findViewById(R.id.emailTextView);
+
+        linkedToTextView = (TextView) findViewById(R.id.linkedToTextView);
         linkedTextView = (TextView) findViewById(R.id.linkedEmailView);
         lastTripTextView = (TextView) findViewById(R.id.lastTripTextView);
         durationTextView = (TextView) findViewById(R.id.durationTextView);
@@ -61,28 +69,28 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void onSignInOutClicked(View view) {
-//        if (currentUser == null) {
-//            // No user is currently signed in; try signing the user in
-//            List<AuthUI.IdpConfig> providers = Arrays.asList(
-//                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
-//
-//            startActivityForResult(AuthUI.getInstance()
-//                            .createSignInIntentBuilder()
-//                            .setAvailableProviders(providers)
-//                            .build(),
-//                    RC_SIGN_IN);
-//        } else {
-//            // A user is currently signed in; sign them out
-//            AuthUI.getInstance().signOut(this)
-//                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                    public void onComplete(@NonNull Task<Void> task) {
-//                                        currentUser = firebaseAuth.getCurrentUser();
-//
-//                                        // Refreshes display to current user depending on whether they're signed in
-//                                        refreshCurrentUserDisplay();
-//                                    }
-//                                });
-//        }
+        if (currentUser == null) {
+            // No user is currently signed in; try signing the user in
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+
+            startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        } else {
+            // A user is currently signed in; sign them out
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            currentUser = firebaseAuth.getCurrentUser();
+
+                            // Refreshes display to current user depending on whether they're signed in
+                            refreshCurrentUserDisplay();
+                        }
+                    });
+        }
     }
 
     public void onLinkAccountClicked(View view) {
@@ -93,21 +101,47 @@ public class SettingsActivity extends AppCompatActivity {
     public void refreshCurrentUserDisplay() {
         if (currentUser == null) {
             nameTextView.setText(R.string.default_sign_in_message);
-            emailTextView.setText("");
+            emailTextView.setText(R.string.noUserSignInText);
 
             signInButton.setText(R.string.sign_in_text);
             linkAccountButton.setVisibility(View.INVISIBLE);
+            linkedTextView.setVisibility(View.INVISIBLE);
+            linkedToTextView.setVisibility(View.INVISIBLE);
+
+            lastTripTextView.setVisibility(View.INVISIBLE);
+            durationTextView.setVisibility(View.INVISIBLE);
+            alarmsTextView.setVisibility(View.INVISIBLE);
         } else {
+            // Sets the user's display name and email address
             nameTextView.setText(currentUser.getDisplayName());
             emailTextView.setText(currentUser.getEmail());
 
-            final DatabaseReference userDatabaseReference = database.getReference("users/" + currentUser.getUid());
-            if (userDatabaseReference.child("child") != null) {
+            // Changes text to "Sign In" as opposed to "Sign Out", and displays link account button
+            signInButton.setText(R.string.sign_out_text);
+            linkAccountButton.setVisibility(View.VISIBLE);
 
-                final DatabaseReference childReference = userDatabaseReference.child("child");
-                childReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
+            // Makes all the linked account views visible
+            linkAccountButton.setVisibility(View.VISIBLE);
+            linkedTextView.setVisibility(View.VISIBLE);
+            linkedToTextView.setVisibility(View.VISIBLE);
+
+            lastTripTextView.setVisibility(View.VISIBLE);
+            durationTextView.setVisibility(View.VISIBLE);
+            alarmsTextView.setVisibility(View.VISIBLE);
+
+            final DatabaseReference userDatabaseReference = database.getReference("users/" +
+                                                                                    currentUser.getUid() + "/child");
+            userDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.getValue() == null) {
+                        // If the user has no linked children, remove the linked user text views
+                        linkedTextView.setText(R.string.linkedAccountsEmpty);
+
+                        lastTripTextView.setVisibility(View.INVISIBLE);
+                        durationTextView.setVisibility(View.INVISIBLE);
+                        alarmsTextView.setVisibility(View.INVISIBLE);
+                    } else {
                         String childUserID = (String) dataSnapshot.getValue();
 
                         // Gets the linked user's email address using their User ID
@@ -125,45 +159,33 @@ public class SettingsActivity extends AppCompatActivity {
 
                         // Displays the recent trip information of the linked user
                         DatabaseReference childTrips = database.getReference("trips/" + childUserID);
-                        if (childTrips != null) {
-                            childTrips.addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    if (dataSnapshot.getValue() == null) {
-                                        Log.d(tag, "Linked user has not gone on any trips");
+                        childTrips.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.getValue() == null) {
+                                    Log.d(tag, "Linked user has not gone on any trips");
 
-                                        // If the linked user has not gone on any trips yet, pass in null for trips
-                                        updateLinkedAccountInfo(null);
-                                    } else {
-                                        for (DataSnapshot childTrip : dataSnapshot.getChildren()) {
-                                            updateLinkedAccountInfo(childTrip);
-                                            break;
-                                        }
+                                    // If the linked user has not gone on any trips yet, pass in null for trips
+                                    updateLinkedAccountInfo(null);
+                                } else {
+                                    for (DataSnapshot childTrip : dataSnapshot.getChildren()) {
+                                        updateLinkedAccountInfo(childTrip);
+                                        break;
                                     }
                                 }
+                            }
 
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-                                }
-                            });
-                        }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                            }
+                        });
                     }
+                }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
-            } else {
-                linkedTextView.setText(R.string.linkedAccountsEmpty);
-
-                // If the user has no linked children, remove the linked user text views
-                lastTripTextView.setVisibility(View.INVISIBLE);
-                durationTextView.setVisibility(View.INVISIBLE);
-                alarmsTextView.setVisibility(View.INVISIBLE);
-            }
-
-            signInButton.setText(R.string.sign_out_text);
-            linkAccountButton.setVisibility(View.VISIBLE);
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
         }
     }
 
@@ -205,18 +227,29 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // WRITE USER TO DATABASE WITH EMAIL ADDRESS
-//        if (requestCode == RC_SIGN_IN) {
-//            IdpResponse response = IdpResponse.fromResultIntent(data);
-//
-//            if (resultCode == RESULT_OK) {
-//                // Successfully signed in
-//                currentUser = FirebaseAuth.getInstance().getCurrentUser();
-//
-//                refreshCurrentUserDisplay();
-//            } else {
-//                // Sign in failed, check response for error code
-//            }
-//        }
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Writes the new user to the database with their email address (so they can be found for linking)
+                final DatabaseReference newUserReference = database.getReference("users/" + currentUser.getUid());
+                newUserReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        newUserReference.child("email").setValue(currentUser.getEmail());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
+                refreshCurrentUserDisplay();
+            } else {
+                // Sign in failed, check response for error code
+            }
+        }
     }
 }
